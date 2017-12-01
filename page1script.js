9912c9b702b1b08635f536d7d96e2208161c15bc
srinivaas <srinivaasganesan@gmail.com>
2017-12-01 14:53:24 -0700

    'Version1'

--- page1script.js
+++ page1script.js
@@ -1,168 +1,191 @@
-rev_actuals = [27,26,28,31,29,31,31,34, 32,33,34,32];
-rev_budget = [28,28,29,30,31,32,32,33,34,34,35,35];
+//Set the month circles
+var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+d3.select('#Month').selectAll('circle').data(month).enter().append('circle')
+	.attr('cx', function(d,i){return i*90+10;})
+	.attr('cy', function(d) {return 15})
+	.attr('r', 7)
+	.classed('normal', 'true')
+	.on('click', function(){ d3.select('#Month').selectAll('circle').classed('selected', false);
+							 d3.select(this).classed('selected', true); });
 
-margin_actuals = [35,34,33,36,32,37,35,32,39,38,36,37]; 
-margin_budget = [36,37,32,38,39,41,38,37,39,39,40, 40];
-month_label = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-var width = 400;
-var height = 400;
-
-var svg = d3.select("#RTrend");
-var xScale = d3.scaleLinear()
-    		   .domain([0, 720])
-			   .range([0, 350]);
-			   
-var yScale = d3.scaleLinear()
-    		   .domain([0, 40])
-			   .range([10, 325]);
-			   
-var yAxisScale = d3.scaleLinear()
-				   .domain([40,0])
-				   .range([10, 325])
-				   .nice();
-						   
-var yAxis = d3.axisLeft();
-yAxis.scale(yAxisScale);
-d3.select("#yAxis")
-	.attr("transform", "translate(" + 40 + "," + 0 +") scale(1,1)")
-	.call(yAxis);			   
-					
-let bars = svg.selectAll("rect").data(rev_actuals);
-bars = bars.enter().append("rect").merge(bars);					
-bars.attr("x", function(d,i){ return xScale(i*60); })
-	.attr("y", function (d, i) { return 25; })
-	.attr("transform", "translate(" + 50 + "," + 350 +") scale(1,-1)")
-	.attr("width", function (d) { return xScale(50); })
-	.attr("height", function(d){ return yScale(d); })
-	.classed("bars", true)
-	.append("svg:title")
-    .text(function(d, i) { return "A: $ " + rev_actuals[i] + " M\nB: $ " + rev_budget[i] + " M"; });
-
-let budget = d3.select("#Budgets").selectAll("rect").data(rev_budget);
-budget = budget.enter().append("rect").merge(budget);					
-budget.attr("x", function(d,i){ return xScale(i*60); })
-	.attr("y", function (d) { return yScale(d); })
-	.attr("transform", "translate(" + 50 + "," + 350 +") scale(1,-1)")
-	.attr("width", function (d) { return xScale(50); })
-	.attr("height", function(d){ return 10; })
-	.style("fill", "#021618");	
-	
-let month = d3.select("#xAxis").selectAll("text").data(month_label);
-month = month.enter().append("text").merge(month);					
-month.attr("x", function(d,i){ return xScale(i*60);})
-	.attr("y", function (d, i) { return 0; })
-	.attr("transform", "translate(" + 50 + "," + 350 +") scale(1,1)")
-	.attr("width", function (d) { return xScale(50); })
-	.attr("height", function(d){ return 50; })
+d3.select('#Month').selectAll('text').data(month).enter().append('text')
+	.attr('x', function(d,i){return i*90;})
+	.attr('y', function(d) {return 40})
 	.text(function(d){return d})
-	.classed("label", true);	
+	.classed('label2', true);
 	
-//Margin Trends	
-svg = d3.select("#MTrend");
-xScale = d3.scaleLinear()
-    		   .domain([0, 720])
-			   .range([0, 350]);
-			   
-yScale = d3.scaleLinear()
-    	   .domain([0, 45])
-		   .range([10, 325]);
-			   
-yAxisScale = d3.scaleLinear()
-			   .domain([45,0])
-			   .range([10, 325])
-			   .nice();
-						   
-yAxis = d3.axisLeft();
-yAxis.scale(yAxisScale);
-d3.select("#MyAxis")
-	.attr("transform", "translate(" + 40 + "," + 0 +") scale(1,1)")
-	.call(yAxis);			   
-					
-bars = svg.selectAll("rect").data(margin_actuals);
-bars = bars.enter().append("rect").merge(bars);					
-bars.attr("x", function(d,i){ return xScale(i*60); })
-	.attr("y", function (d, i) { return 25; })
-	.attr("transform", "translate(" + 50 + "," + 350 +") scale(1,-1)")
-	.attr("width", function (d) { return xScale(50); })
-	.attr("height", function(d){ return yScale(d); })
-	.classed("bars", true)
-	.append("svg:title")
-    .text(function(d, i) { return "A: " + margin_actuals[i] + " %\nB: " + margin_budget[i] + " %"; });
+var data = [];
+var associate_data = [];
 
-budget = d3.select("#MBudgets").selectAll("rect").data(margin_budget);
-budget = budget.enter().append("rect").merge(budget);					
-budget.attr("x", function(d,i){ return xScale(i*60); })
-	.attr("y", function (d) { return yScale(d); })
-	.attr("transform", "translate(" + 50 + "," + 350 +") scale(1,-1)")
-	.attr("width", function (d) { return xScale(50); })
-	.attr("height", function(d){ return 10; })
-	.style("fill", "#021618");	
-	
-month = d3.select("#MxAxis").selectAll("text").data(month_label);
-month = month.enter().append("text").merge(month);					
-month.attr("x", function(d,i){ return xScale(i*60);})
-	.attr("y", function (d, i) { return 0; })
-	.attr("transform", "translate(" + 50 + "," + 350 +") scale(1,1)")
-	.attr("width", function (d) { return xScale(50); })
-	.attr("height", function(d){ return 50; })
-	.text(function(d){return d})
-	.classed("label", true);
-	
-//Populate the table
-var VH = [
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,0,0,0,0]
-];
+d3.csv("Associate.csv", function (error, csvData) {   
+	 setAssociateData(csvData);
+});
 
 d3.csv("Summary.csv", function (error, csvData) {   
-	 var sum = 0;
-     csvData.forEach(function (d, i) {
-         VH[getIndex(d.Vertical)][getIndex(d.Horizontal)] += parseFloat(d.Revenue)/1000000;
-     });	
+	 setData(csvData);
+});
 
-	console.log(VH[0]);			
+function setData(value){
+	this.data = value;
+	updateTable(this.data, 'Jan', 'Feb');
+	updateWalks(this.data, "Jan", "Feb", ".*", ".*", ".*", ".*");
+}
+
+function setAssociateData(value){
+	this.associate_data = value;
+}
+
+function updateTable(data, prev, curr){
+	console.log(data[0]);
+	//Populate the table
+	var VH = [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]];
+	var VH_Margin = [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]];
+	var VH_prev = [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]];
+	var VH_prevMargin = [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]];
+	var t;
 	
-	d3.select("#BFS")
-	.selectAll("td").data(VH[0])
-	.enter().append("td")
-	.text(function(d) { return '$ '+d.toFixed(2) + ' M'})
-	.classed("value", true);
+	data.forEach(function (d, i) {
+		if(d.Month==curr){	
+			t = parseFloat(d.Revenue)/1000000;
+			VH[getIndex(d.Vertical)][getIndex(d.Horizontal)] += t;
+			VH[getIndex(d.Vertical)][5] += t;
+			VH[6][getIndex(d.Horizontal)] += t;
+			VH[6][5] += t;
+			
+			t = parseFloat(d.Margin)/1000000;
+			VH_Margin[getIndex(d.Vertical)][getIndex(d.Horizontal)] += t;
+			VH_Margin[getIndex(d.Vertical)][5] += t;
+			VH_Margin[6][getIndex(d.Horizontal)] += t;
+			VH_Margin[6][5] += t;
+		}
+		
+		if(d.Month==prev){	
+			t = parseFloat(d.Revenue)/1000000;
+			VH_prev[getIndex(d.Vertical)][getIndex(d.Horizontal)] += t;
+			VH_prev[getIndex(d.Vertical)][5] += t;
+			VH_prev[6][getIndex(d.Horizontal)] += t;
+			VH_prev[6][5] += t;
+			
+			t = parseFloat(d.Margin)/1000000;
+			VH_prevMargin[getIndex(d.Vertical)][getIndex(d.Horizontal)] += t;
+			VH_prevMargin[getIndex(d.Vertical)][5] += t;
+			VH_prevMargin[6][getIndex(d.Horizontal)] += t;
+			VH_prevMargin[6][5] += t;
+		}
+     });
+
+	 var tableValues = [];
+	 var prevRev, currRev, prevMargin, currMargin;
+	 
+	 for(var i=0; i<7; i++){
+		 var temp = [];
+		 for(var j=0; j<6; j++){
+			 var duplex = {'revenue':0, 'margin':0, 'color':0};
+			 currRev = VH[i][j];
+			 duplex.Revenue = currRev;
+			 
+			 currMargin = VH_Margin[i][j]/VH[i][j]*100;
+			 duplex.Margin = currMargin;
+			 
+			 prevRev = VH_prev[i][j];
+			 prevMargin = VH_prevMargin[i][j]/VH_prev[i][j]*100;
+			 
+			 if(currRev > prevRev) duplex.color += 2;
+			 if(currMargin > prevMargin) duplex.color += 1;
+			 
+			 temp.push(duplex);
+		 }
+		 
+		 tableValues.push(temp);
+	 }
+
+	var Maindata = this.data;
+	var currMonth, prevMonth;
+	var vertical;
 	
-	d3.select("#Healthcare")
-	.selectAll("td").data(VH[1])
-	.enter().append("td")
-	.text(function(d) { return '$ '+d.toFixed(2) + ' M'})
-	.classed("value", true);
+	currMonth = 'Feb';
+	prevMonth = 'Jan';
 	
-	d3.select("#Technology")
-	.selectAll("td").data(VH[2])
-	.enter().append("td")
-	.text(function(d) { return '$ '+d.toFixed(2) + ' M'})
-	.classed("value", true);
+	//Horizontal
+	d3.select('#Horizontal').selectAll('th')
+	  .on('click', function(d,i) {updateWalks(Maindata, prevMonth, currMonth, '.*', i-1, '.*', '.*');});
+		
+	//BFS
+	vertical = d3.select('#BFS');
+	vertical.select('th').on('click', function() {updateWalks(Maindata, prevMonth, currMonth, 'BFS', '.*', '.*', '.*')});
+	vertical.selectAll("td").data(tableValues[0])
+			.enter().append('td')
+			.text(function(d) { return '$'+d.Revenue.toFixed(2)+'\xa0\xa0\xa0'+d.Margin.toFixed(1)+'%'})
+			.on('click', function(d,i){ updateWalks(Maindata, prevMonth, currMonth, 'BFS', i, '.*', '.*'); });
+		
+	//Healthcare	
+	vertical = d3.select('#Healthcare');
+	vertical.select('th').on('click', function() {updateWalks(Maindata, prevMonth, currMonth, 'Healthcare', '.*', '.*', '.*')});
+	vertical.selectAll("td").data(tableValues[1])
+			.enter().append('td')
+			.text(function(d) { return '$'+d.Revenue.toFixed(2)+'\xa0\xa0\xa0'+d.Margin.toFixed(1)+'%'})
+			.on('click', function(d,i){ updateWalks(Maindata, prevMonth, currMonth, 'Healthcare', i, '.*', '.*'); });
+			
+	//Technology	
+	vertical = d3.select('#Technology');
+	vertical.select('th').on('click', function() {updateWalks(Maindata, prevMonth, currMonth, 'Technology', '.*', '.*', '.*')});
+	vertical.selectAll("td").data(tableValues[2])
+			.enter().append('td')
+			.text(function(d) { return '$'+d.Revenue.toFixed(2)+'\xa0\xa0\xa0'+d.Margin.toFixed(1)+'%'})
+			.on('click', function(d,i){ updateWalks(Maindata, prevMonth, currMonth, 'Technology', i, '.*', '.*'); });
 	
-	d3.select("#Manlog")
-	.selectAll("td").data(VH[3])
-	.enter().append("td")
-	.text(function(d) { return '$ '+d.toFixed(2) + ' M'})
-	.classed("value", true);
+	//Manlog	
+	vertical = d3.select('#Manlog');
+	vertical.select('th').on('click', function() {updateWalks(Maindata, prevMonth, currMonth, 'Manlog', '.*', '.*', '.*')});
+	vertical.selectAll("td").data(tableValues[3])
+			.enter().append('td')
+			.text(function(d) { return '$'+d.Revenue.toFixed(2)+'\xa0\xa0\xa0'+d.Margin.toFixed(1)+'%'})
+			.on('click', function(d,i){ updateWalks(Maindata, prevMonth, currMonth, 'Manlog', i, '.*', '.*'); });
 	
-	d3.select("#Retail")
-	.selectAll("td").data(VH[4])
-	.enter().append("td")
-	.text(function(d) { return '$ '+d.toFixed(2) + ' M'})
-	.classed("value", true);
+	//Retail	
+	vertical = d3.select('#Retail');
+	vertical.select('th').on('click', function() {updateWalks(Maindata, prevMonth, currMonth, 'Retail', '.*', '.*', '.*')});
+	vertical.selectAll("td").data(tableValues[4])
+			.enter().append('td')
+			.text(function(d) { return '$'+d.Revenue.toFixed(2)+'\xa0\xa0\xa0'+d.Margin.toFixed(1)+'%'})
+			.on('click', function(d,i){ updateWalks(Maindata, prevMonth, currMonth, 'Retail', i, '.*', '.*'); });
+			
+	//Others	
+	vertical = d3.select('#Others');
+	vertical.select('th').on('click', function() {updateWalks(Maindata, prevMonth, currMonth, 'Others', '.*', '.*', '.*')});
+	vertical.selectAll("td").data(tableValues[5])
+			.enter().append('td')
+			.text(function(d) { return '$'+d.Revenue.toFixed(2)+'\xa0\xa0\xa0'+d.Margin.toFixed(1)+'%'})
+			.on('click', function(d,i){ updateWalks(Maindata, prevMonth, currMonth, 'Others', i, '.*', '.*'); });
+			
+	//Total	
+	vertical = d3.select('#Total');
+	vertical.select('th').on('click', function(d,i) {updateWalks(Maindata, prevMonth, currMonth, '.*', '.*', '.*', '.*')});
+	vertical.selectAll('td').data(tableValues[6])
+			.enter().append('td')
+			.text(function(d) { return '$'+d.Revenue.toFixed(2)+'\xa0\xa0\xa0'+d.Margin.toFixed(1)+'%'})
+			.on('click', function(d,i){ updateWalks(Maindata, prevMonth, currMonth, '.*', i, '.*', '.*'); });	
+
+	d3.select('#summary').selectAll('td').filter(function(d) {return d.color==0}).classed('type1', true);			
+	d3.select('#summary').selectAll('td').filter(function(d) {return d.color==1}).classed('type2', true);			
+	d3.select('#summary').selectAll('td').filter(function(d) {return d.color==2}).classed('type3', true);			
+	d3.select('#summary').selectAll('td').filter(function(d) {return d.color==3}).classed('type4', true);			
+}
+
+function updateWalks(data, prev, curr, vert, horiz, client, project){
+	associate_data = this.associate_data;
 	
-	d3.select("#Others")
-	.selectAll("td").data(VH[5])
-	.enter().append("td")
-	.text(function(d) { return '$ '+d.toFixed(2) + ' M'})
-	.classed("value", true);
-});	
+	if(horiz==0) horiz = 'QA';
+	else if(horiz==1) horiz = 'SAP';
+	else if(horiz==2) horiz = 'HCM';
+	else if(horiz==3) horiz = 'Analytics';
+	else if(horiz==4) horiz = 'Cloud';
+	else horiz = '.*';
+	
+	updateRevWalk(data, prev, curr, vert, horiz, client, project);
+	updateMarginWalk(data, prev, curr, vert, horiz, client, project);
+	updatePPMWalk(associate_data, prev, curr, vert, horiz, client, project);
+}
 
 function getIndex(s){
 	if(s=="BFS") return 0;
---
+++ updatePPMWalk.js
@@ -0,0 +1,147 @@
+function updatePPMWalk(data, prev, curr, vert, horiz, client, project){
+	var table = [];
+	
+	temp = {'id':'Anlst', 'prev_off':0, 'curr_off':0, 'prev_on':0, 'curr_on':0, 'on_diff':0, 'off_diff':0}; table.push(temp);
+	temp = {'id':'Assoc', 'prev_off':0, 'curr_off':0, 'prev_on':0, 'curr_on':0, 'on_diff':0, 'off_diff':0}; table.push(temp);
+	temp = {'id':'Sr Assoc', 'prev_off':0, 'curr_off':0, 'prev_on':0, 'curr_on':0, 'on_diff':0, 'off_diff':0}; table.push(temp);
+	temp = {'id':'Manager', 'prev_off':0, 'curr_off':0, 'prev_on':0, 'curr_on':0, 'on_diff':0, 'off_diff':0}; table.push(temp);
+	temp = {'id':'Sr Mgr', 'prev_off':0, 'curr_off':0, 'prev_on':0, 'curr_on':0, 'on_diff':0, 'off_diff':0}; table.push(temp);
+	temp = {'id':'Director', 'prev_off':0, 'curr_off':0, 'prev_on':0, 'curr_on':0, 'on_diff':0, 'off_diff':0}; table.push(temp);
+	temp = {'id':'Subcon', 'prev_off':0, 'curr_off':0, 'prev_on':0, 'curr_on':0, 'on_diff':0, 'off_diff':0}; table.push(temp);
+	
+	data.forEach(function (d, i) {
+		if(d.Month==curr && d.Vertical.match(vert) && d.Horizontal.match(horiz) && d.Client.match(client) && d['Project Id'].match(project)){
+			if(d['Onsite/Offshore']=='Onsite') table[getGrade(d.Grade)].curr_on += parseInt(d.Count);
+			else table[getGrade(d.Grade)].curr_off += parseInt(d.Count);
+		}
+		if(d.Month==prev && d.Vertical.match(vert) && d.Horizontal.match(horiz) && d.Client.match(client) && d['Project Id'].match(project)){
+			if(d['Onsite/Offshore']=='Onsite') table[getGrade(d.Grade)].prev_on += parseInt(d.Count);
+			else table[getGrade(d.Grade)].prev_off += parseInt(d.Count);
+		}
+    });
+	
+	var min, max;
+	for(var i=0; i<6; i++){
+		table[i].on_diff = table[i].curr_on - table[i].prev_on;
+		table[i].off_diff = table[i].curr_off - table[i].prev_off;
+	}
+	
+	//Plot onsite variation
+	min = Number.MAX_VALUE;
+	max = Number.MIN_VALUE;
+	for(var i=0; i<7; i++){
+		if(min > table[i].on_diff) min = table[i].on_diff;
+		if(max < table[i].on_diff) max = table[i].on_diff;
+	}
+	
+	//Draw the chart
+	var ymax = 250;
+	var xmax = 200;
+	
+	//Set yScale
+	var range;
+	if(min < 0) range = max - min;
+	else range = max;
+	
+	console.log(min, max, range);
+	
+	var xScale = d3.scaleLinear().domain([0, 420]).range([0, xmax-5]);
+	var yScale = d3.scaleLinear().domain([min, max]).range([1,55]);
+	var hScale = d3.scaleLinear().domain([0, range]).range([1,55]);
+	
+	var pplwalk = d3.select('#onsite');
+	pplwalk = pplwalk.selectAll('rect').data(table);
+	pplwalk = pplwalk.enter().append('rect').merge(pplwalk);		
+	
+	pplwalk.attr('x', function(d,i){ return xScale(i*60); })
+		.attr('y', function (d, i) { if(d.on_diff>0) return yScale(0); return yScale(d.on_diff);})
+		.attr('transform', 'translate(' + 5 + ',' + 110 +') scale(1,-1)')
+		.attr('width', function (d) { return xScale(50); })
+		.attr('height', function(d){ return hScale(Math.abs(d.on_diff)); })
+		.classed('bars', true);
+	
+	pplwalk.classed('incr', false);
+	pplwalk.classed('drop', false);
+	pplwalk.classed('incr', true);
+	pplwalk.filter(function(d){ return d.on_diff<0}).classed('drop', true);	
+	
+	pplwalk = d3.select('#PplWalkLabel').selectAll('text').data(table);
+	pplwalk = pplwalk.enter().append('text').merge(pplwalk);					
+	pplwalk.text(function(d){ return d.on_diff})
+		.attr('transform', function(d,i){ 
+			var t = 105-yScale(d.on_diff);
+			if(d.on_diff<0) t = 105 - yScale(0);
+			
+			return 'translate(' + (xScale(i*60)+20) + ',' + t +')'})
+		.classed('label', true)
+		.attr('text-anchor','end');
+		
+	//Offshore Chart	
+	min = Number.MAX_VALUE;
+	max = Number.MIN_VALUE;
+	for(var i=0; i<7; i++){
+		if(min > table[i].off_diff) min = table[i].off_diff;
+		if(max < table[i].off_diff) max = table[i].off_diff;
+	}
+	
+	//Draw the chart
+	ymax = 250;
+	xmax = 200;
+	
+	//Set yScale
+	var range;
+	if(min < 0) range = max - min;
+	else range = max;
+	
+	console.log(table);
+	console.log(min, max, range);
+	
+	xScale = d3.scaleLinear().domain([0, 420]).range([0, xmax-5]);
+	yScale = d3.scaleLinear().domain([min, max]).range([1,55]);
+	hScale = d3.scaleLinear().domain([0, range]).range([1,55]);
+	
+	pplwalk = d3.select('#offshore');
+	pplwalk = pplwalk.selectAll('rect').data(table);
+	pplwalk = pplwalk.enter().append('rect').merge(pplwalk);		
+	
+	pplwalk.attr('x', function(d,i){ return xScale(i*60); })
+		.attr('y', function (d, i) { if(d.off_diff>0) return yScale(0); return yScale(d.off_diff);})
+		.attr('transform', 'translate(' + 5 + ',' + 200 +') scale(1,-1)')
+		.attr('width', function (d) { return xScale(50); })
+		.attr('height', function(d){ return hScale(Math.abs(d.off_diff)); })
+		.classed('bars', true);
+	
+	pplwalk.classed('incr', false);
+	pplwalk.classed('drop', false);
+	pplwalk.classed('incr', true);
+	pplwalk.filter(function(d){ return d.off_diff<0}).classed('drop', true);
+
+	pplwalk = d3.select('#offlabel').selectAll('text').data(table);
+	pplwalk = pplwalk.enter().append('text').merge(pplwalk);					
+	pplwalk.text(function(d){ return d.off_diff})
+		.attr('transform', function(d,i){ 
+			var t = 195-yScale(d.off_diff);
+			if(d.off_diff<0) t = 195 - yScale(0);
+			
+			return 'translate(' + (xScale(i*60)+20) + ',' + t +')'})
+		.classed('label', true)
+		.attr('text-anchor','end');	
+	
+	//ASSIGN THE LABEL
+	revwalk = d3.select('#PplWalkId').selectAll('text').data(table);
+	revwalk = revwalk.enter().append('text').merge(revwalk);					
+	revwalk.text(function(d){ return d.id})
+		.attr('transform', function(d,i){ return 'translate(' + (xScale(i*60)+20) + ',' + (ymax-45) +')' + ' rotate(-60)'})
+		.classed('label', true)
+		.attr('text-anchor','end');
+}
+
+function getGrade(grade){
+	if(grade=='Analyst') return 0;
+	if(grade=='Associate') return 1;
+	if(grade=='Senior Associate') return 2;
+	if(grade=='Manager') return 3;
+	if(grade=='Senior Manager') return 4;
+	if(grade=='Director') return 5;
+	if(grade=='Contractor') return 6;
+}
